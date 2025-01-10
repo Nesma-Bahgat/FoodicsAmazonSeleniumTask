1943b7f811b68f20245876edbcff2d140baebf95
Nesma-Bahgat <36860733+Nesma-Bahgat@users.noreply.github.com>
2025-01-10 03:10:21 +0200

    Update NavigationBar.java
    Remove all thread.sleep

--- src/main/java/NavigationBar.java
+++ src/main/java/NavigationBar.java
@@ -3,6 +3,7 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NavigationBar extends BasePage {
@@ -15,10 +16,9 @@ public class NavigationBar extends BasePage {
         wait = new WebDriverWait(driver, Duration.ofSeconds(10000));
     }
 
-    public void goToCartPage() throws InterruptedException {
-        Thread.sleep(2000);
+    public void goToCartPage() {
         scrollToTheTopOfThePage();
-        cartButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(cartButton)).click();
     }
 
 }
